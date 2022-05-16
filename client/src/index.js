b02505e5eddfc87457a89bbec717a4c2f7c08eec
Harkins12345 <49042556+Harkins12345@users.noreply.github.com>
2022-05-16 10:24:24 +0100

    Set up store and redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import store from './store'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
