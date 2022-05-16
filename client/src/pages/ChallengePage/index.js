ec9ea5a814f217c0fe9fe25a1773fe2a87792d65
kharileigh <kharileighmiller@gmail.com>
2022-05-16 14:19:18 +0100

    CREATED CONTAINER FOR CHALLENGE PAGE

---
+++ client/src/pages/ChallengePage/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+
+function ChallengePage() {
+
+
+
+
+    return (
+        <Container fluid>
+            <Row>
+
+                {/* ----- ONLINE USER COLUMN ----- */}
+                <Col>
+
+                <h1>Currently Online</h1>
+                </Col>
+
+                {/* ----- SELECT CATEGORY & LEVEL ----- */}
+                <Col>
+
+                    
+
+                </Col>
+            </Row>
+
+
+
+
+
+        </Container>
+    )
+
+
+}
+
+
+
+
+export default ChallengePage;
