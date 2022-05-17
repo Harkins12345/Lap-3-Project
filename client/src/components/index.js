af6c56e77bb17ec3302c80fdc0d12adc5b6b6e50
kharileigh <kharileighmiller@gmail.com>
2022-05-17 22:10:18 +0100

    CREATED MODAL COMPONENT - need to add functionality

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as NavBar } from './NavBar';
 export { default as LoginForm } from './LoginForm';
 export { default as RegisterForm } from './LoginForm';
+export { default as Modal } from './Modal';
