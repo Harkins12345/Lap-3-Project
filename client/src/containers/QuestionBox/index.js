d7918ca424ec7fb56c019a29d5e1b134980d827a
Harkins12345 <49042556+Harkins12345@users.noreply.github.com>
2022-05-17 20:59:51 +0100

    Mutliple update, validation, socket listeners added etc

--- client/src/containers/QuestionBox/index.js
+++ client/src/containers/QuestionBox/index.js
@@ -1,15 +1,12 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
 function QuestionBox() {
     const socket = useSelector(state => state.socket);
 
-    const [questionData, setQuestionData] = useState([])
+    const [questionData, setQuestionData] = useState([]);
     const [select, setSelect] = useState({
         correct: null,
         selected: false
@@ -19,13 +16,13 @@ function QuestionBox() {
 
     }
 
-    socket.on('getQuestion', data => setQuestionData(data));
+    socket.on('sendQuestion', data => setQuestionData(data));
 
     return (
         <Card>
-            { questionData.question ? <Card.Header>{questionData.question}</Card.Header> : <Card.Header>Loading...</Card.Header> }
+            { questionData.question ? <Card.Header>{questionData.question}</Card.Header> : <Card.Header>Loading Question...</Card.Header> }
             <Card.Body>
-                {questionData.answers ? questionData.answers.map(answer => <Button variant="outline-dark">{answer}</Button>) : <h1>Loading Questions...</h1>}
+                {questionData.answers ? questionData.answers.map((answer, index) => <Button variant="outline-dark">{answer}</Button>) : <h1>Loading Answers...</h1>}
             </Card.Body>
         </Card>
     );
