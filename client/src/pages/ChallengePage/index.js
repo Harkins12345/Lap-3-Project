b85e12720e006eb6908dabf7677eafb59ff396c4
kharileigh <kharileighmiller@gmail.com>
2022-05-17 22:09:32 +0100

    UPDATE CHALLENGE FUNCTIONALITY TO DISPATCH TO SOCKET

--- client/src/pages/ChallengePage/index.js
+++ client/src/pages/ChallengePage/index.js
@@ -1,42 +1,38 @@
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Card, Dropdown, DropdownButton, Button, Stack, ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
 import './style.css';
 
 import avatar from '../../images/avatar.png';
 import avatar1 from '../../images/avatar1.png';
+// import Modal from '../../components/Modal';
+
 
 
 function ChallengePage() {
 
     const socket = useSelector(state => state.socket);
 
+    const [ username, setUsername ] = useState("");
     const [ category, setCategory ] = useState("");
     const [ difficulty, setDifficulty ] = useState("");
+    const [ selectedUser, setSelectedUser ] = useState("");
 
+    const dispatch = useDispatch();
+   
+  function handleSubmit()  {
+       const data = {
+           currentUser: username,
+           challengedUser: selectedUser,
+           category: category,
+           difficulty: difficulty
+       }
 
-
-    // --------- UPDATES STATE OF CATEGORY & DIFICULTY ----- //
-    const updateCategory = e => {
-        const selectedCategory = e.target.value
-        setCategory(selectedCategory);
-    } 
-
-    const updateDifficulty = e => {
-        const selectedDifficulty = e.target.value
-        setDifficulty(selectedDifficulty);
-    } ;
-
-    // ---------- DISPATCHES THE RESULTS USING ACTIONS TO STORE ------ //
-    // ---------- NEED TO DEFINE & IMPORT ACTIONS ------ //
-    const handleSubmit = ({ selectedItems }) => {
-       //socket.emit()
-        
+       dispatch(socket.emit("sendRequestChallenge", data))
+       
     }
 
-    // socket.emit()
-
-  
+   
     const radios = [
       { name: 'EASY', value: '1', className:"easy-button"},
       { name: 'MEDIUM', value: '2', className:"medium-button"},
@@ -46,14 +42,20 @@ function ChallengePage() {
 
 
     return (
+
+        // TERNARY STATEMENT --> if pending then show modal box (state.challengePending display modal if not display entire page)
+        // modal box to be created 
+
         <div className="main-container">
+
+            {/* <Modal /> */}
     
 
             {/* ----- ONLINE USER COLUMN ----- */}
              <div className="left-container">
                 
                 {/* ----- ONLINE USERS ----- */}
-                <Stack direction="horizontal" gap={2}>
+                <Stack value={selectedUser} direction="horizontal" gap={2}>
                     <div className="stack">
                         <Card className="card">
                             <Card.Body className="card-body">
@@ -89,13 +91,14 @@ function ChallengePage() {
                 {/* ----- SELECT CATEGORY ----- */}
                 <div className="category-row row">
                             
-                    <DropdownButton onClick={updateCategory} value={category} id="category-button" title="CATEGORY" size="lg" className='d-grid gap-2'>
+                    <DropdownButton onClick={category} id="category-button" title="CATEGORY" size="lg" className='d-grid gap-2'>
 
-                        <Dropdown.Item as="button">Science: Computer</Dropdown.Item>
-                        <Dropdown.Item as="button">Science: Mathematics</Dropdown.Item>
-                        <Dropdown.Item as="button">Science & Nature</Dropdown.Item>
-                        <Dropdown.Item as="button">History</Dropdown.Item>
-                        <Dropdown.Item as="button">Geography</Dropdown.Item>
+                        <Dropdown.Item as="button" value={17} >Science & Nature</Dropdown.Item>
+                        <Dropdown.Item as="button" value={18} >Science: Computer</Dropdown.Item>
+                        <Dropdown.Item as="button" value={19} >Science: Mathematics</Dropdown.Item>
+                        <Dropdown.Item as="button" value={22} >Geography</Dropdown.Item>
+                        <Dropdown.Item as="button" value={23} >History</Dropdown.Item>
+                        
                                 
                     </DropdownButton >
                         
@@ -104,7 +107,7 @@ function ChallengePage() {
                  {/* ----- SELECT LEVEL ----- */}
                 <div className="level-row row">
                         
-                    <ToggleButtonGroup onClick={updateDifficulty} value={difficulty} name="toggle" size="lg">
+                    <ToggleButtonGroup onClick={difficulty} name="toggle" size="lg">
                         {radios.map((radio, idx) => (
                         <ToggleButton
                             key={idx}
